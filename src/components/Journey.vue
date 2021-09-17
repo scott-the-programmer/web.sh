f162f5d35daf0434dff2534d9d214d5e93fac023
Scott Murray <scottam.coder@gmail.com>
2021-09-17 19:20:14 +1200

    create journey section

---
+++ src/components/Journey.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="pt-10">
+    <menu-header heading="My Journey"></menu-header>
+    <journey-item
+      :imageSvg="require('@/assets/icons/zip.jpg')"
+      date="Zip 2019 - present"
+      description="A wild ride at Zip! Currently changing the game at a fast-paced, high performing, fintech company"
+    ></journey-item>
+    <journey-item
+      :imageSvg="require('@/assets/icons/iqvia.jpg')"
+      date="IQVIA 2016 - 2019"
+      description=".NET based role solving CRM problems with a large emphasis on data partitioning and synchronizing"
+    ></journey-item>
+    <journey-item
+      :imageSvg="require('@/assets/icons/dxc.jpg')"
+      date="DXC Technology 2015"
+      description="Working with local NZ businesses to create bespoke integrations with Dynamics AX"
+    ></journey-item>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MenuHeader from '@/components/MenuHeader.vue';
+import JourneyItem from '@/components/JourneyItem.vue';
+
+export default defineComponent({
+  components: { MenuHeader, JourneyItem },
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/JourneyItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="pb-16 lg:pb-0 mt:5">
+    <div class="xl:flex xl:mx-auto">
+      <div class="h-40 w-40 m-auto xl:m-1 block mb-6 lg:mb-0">
+        <img
+          class="max-h-full max-w-full m-auto w-full h-full rounded-md"
+          :src="imageSvg"
+        />
+      </div>
+      <p
+        class="
+          px-7
+          lg:w-2/6
+          m-auto
+          xl:my-auto
+          text-sm text-blog-text
+          lg:text-xl
+          text-center
+          xl:text-left
+          pb-6
+          lg:pb-0
+        "
+      >
+        {{ date }}
+      </p>
+      <p
+        class="
+          px-7
+          lg:w-3/6
+          m-auto
+          xl:my-auto
+          text-blog-text text-lg
+          lg:text-xl
+          text-center
+          xl:text-left
+        "
+      >
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    imageSvg: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style></style>
