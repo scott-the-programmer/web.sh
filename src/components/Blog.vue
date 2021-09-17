1f24b883bd064170e0646ca495c8f6c82a654f58
Scott Murray <scottam.coder@gmail.com>
2021-09-17 19:20:24 +1200

    update blog to use new sections

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -3,8 +3,8 @@
     <div class="m-auto xl:w-1/2">
       <cloud></cloud>
       <web></web>
-      <dumb-projects></dumb-projects>
-      <resume></resume>
+      <programming></programming>
+      <journey></journey>
     </div>
   </section>
 </template>
@@ -12,12 +12,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cloud from '@/components/Cloud.vue';
-import DumbProjects from '@/components/Projects.vue';
 import Web from '@/components/Web.vue';
-import Resume from '@/components/Resume.vue';
+import Journey from '@/components/Journey.vue';
+import Programming from '@/components/Programming.vue';
 
 export default defineComponent({
-  components: { Cloud, Web, DumbProjects, Resume },
+  components: { Cloud, Web, Programming, Journey },
 });
 </script>
 
