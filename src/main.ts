38a2505dc763fa4592ac374debef7b111682e479
Scott Murray <scottam.coder@gmail.com>
2021-09-18 14:01:32 +1200

    remove unused store import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
-import store from './store';
 import './index.css';
 
-createApp(App).use(store).use(vuetify).mount('#app');
+createApp(App).use(vuetify).mount('#app');
